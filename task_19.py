ff5211629468b4400e765fbe89be9b5796df2ff3
Tatiana <tderun80@gmail.com>
2016-11-22 22:28:22 +0200

    Update task_19.py

--- task_19.py
+++ task_19.py
@@ -1,7 +1,10 @@
+k = 0
 
 for i in range(1,101):
     for j in range(2,i):
         if i%j==0:
-            break
-        else:
-            print(i)
+            k = k + 1
+    if k ==0:
+        print(i)
+    else:
+        k = 0
