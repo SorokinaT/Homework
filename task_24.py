24a0aa8f1bd01184e8f6aa675e26158492621e15
SorokinaT <tderun80@gmail.ru>
2016-11-27 14:05:51 +0200

    corrected

--- task_24.py
+++ task_24.py
@@ -1,9 +1,9 @@
 
-def sum_os_numbers(lst):
+def find_difference_of_sum_of_odds_and_evens(lst):
     sum_of_evens = 0
     sum_of_odd = 0
     for i in range(len(lst)):
-        if lst[i]%2:
+        if lst[i]%2==0:
             sum_of_evens = sum_of_evens + lst[i]
         else:
             sum_of_odd = sum_of_odd + lst[i]
@@ -11,4 +11,4 @@ def sum_os_numbers(lst):
     return diff
 
 lst = [5.25,10,1,3,9,4]
-print("The difference sums of even and odd in lst %s = %.2f" % (lst, sum_os_numbers(lst)))
+print("The difference sums of even and odd in lst %s = %.2f" % (lst, find_difference_of_sum_of_odds_and_evens(lst)))
